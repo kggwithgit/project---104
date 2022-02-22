3623c87105856ce99102b2158634c7cda88a7531
kggwithgit <100148840+kggwithgit@users.noreply.github.com>
2022-02-22 16:01:49 +0100

    Add files via upload

---
+++ Image_recognizer project - 103/Image_recognizer/main.js
@@ -0,0 +1,17 @@
+//https://teachablemachine.withgoogle.com/models/i1O9xGSoV/
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src = "'+data_uri+'"/>';
+    });
+}
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/i1O9xGSoV/model.json', modelLoaded);
